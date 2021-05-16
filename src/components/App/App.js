1ff84ccdaf32d1b4c367e9818bd3596840efe13d
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-05-16 15:20:15 +0100

    fix grid layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,6 @@
 import React, {useState} from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-
-
-// import {HiOutlineMenuAlt4, HiOutlineX} from 'react-icons/hi'
-
 import Logo from '../Logo'
 import Nav from '../Nav'
 import QuizAdd from '../QuizAdd'
@@ -14,35 +10,23 @@ import CreatePage from '../CreatePage'
 
 
 const App =  () => {
-
   const [currentQuiz, setCurrentQuiz] = useState({});
-
   const handleClick = (item) => {
     setCurrentQuiz(item)
   }
 
   return (
-
     <Router>
-
       <div className={`bg-blueGray-50 min-h-screen border-t-2 border-fuchsia-600 space-y-4 flex flex-col`}>
-
         <header className={`sticky top-0 border-b border-fuchsia-200 p-4 flex items-center justify-between bg-white shadow z-30`}>
-
             <Logo className={`border-2 rounded-bl-2xl rounded-t-2xl border-fuchsia-600 px-2 py-1 text-fuchsia-600 hover:bg-fuchsia-600 hover:text-white text-xl font-sans font-bold cursor-pointer`}>Q</Logo>
             <Nav className={`flex space-x-4`}>
               {/* <Link className={`px-4 py-2 cursor-pointer hover:bg-fuchsia-200 focus:bg-fuchsia-200 rounded font-semibold text-fuchsia-600`} to="/create">CREATE
               </Link> */}
               <Link className={`bg-fuchsia-600 rounded text-fuchsia-50 px-4 py-2 cursor-pointer hover:bg-fuchsia-700 focus:bg-fuchsia-700 font-semibold`} to="/create">CREATE
               </Link>
-
-              {/* <HiOutlineMenuAlt4 className={`text-xl w-8 h-8 text-fuchsia-600`} />*/}
-              {/* <HiOutlineX  className={`text-xl w-8 h-8 text-fuchsia-600`} /> */}
-
             </Nav>
-
         </header>
-
 
         <main className={`px-4 w-full md:mx-auto md:max-w-4xl lg:max-w-5xl xl:max-w-6xl flex-1 flex flex-col`}>
           { <Switch>
@@ -61,15 +45,11 @@ const App =  () => {
           </Switch> }
         </main>
 
-
-
         <footer className={`p-4 space-y-4 flex flex-col justify-center bg-fuchsia-100 shadow-inner `}>
           <div className={`bg-pink-200`}></div>
           <small className={`text-fuchsia-500 self-center`}>With <span className={`text-red-600`}>&hearts;</span> from <a className={`border-b-2 border-fuchsia-600 text-fuchsia-600 `}  target="_blank" rel="noreferrer" href="https://github.com/mikhaelnwankwo/qui-zap">Quizap HQ</a></small>
         </footer>
-
       </div>
-
     </Router>
   );
 };
--- src/components/QuizList/QuizList.js
+++ src/components/QuizList/QuizList.js
@@ -4,7 +4,7 @@ import React from "react";
 const QuizList = ({children}) => {
   return (
     <div>
-      <span className={`flex flex-row-reverse flex-wrap-reverse gap-4 space-x-1 justify-center `}>
+      <span className={`flex flex-row-reverse flex-wrap-reverse gap-4 m-4 justify-center`}>
       {children}
       </span>
     </div>
