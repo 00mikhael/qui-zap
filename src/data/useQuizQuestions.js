6e341b3092565bd857c5395fbb6df4af4c733487
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-05-19 15:42:32 +0100

    disable refresh for quiztions

--- src/data/useQuizQuestions.js
+++ src/data/useQuizQuestions.js
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
 const useQuizQuestions = (quiz_id) => {
-    const { data, mutate } = useSWR('/api/quizQuestions?id=' + quiz_id);
+    const { data, mutate } = useSWR('/api/quizQuestions?id=' + quiz_id, {refreshInterval = 0});
 
     const { questions } = data;
 
