3455f85c25ad5d245c1342036f9f443eadbb1c8e
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-05-19 22:59:56 +0100

    .

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,7 +5,7 @@ import styles from './Board.module.css'
 const Board = ({left, right}) => {
   return (
     <div className={`${styles.filter}`}>
-      <div style={{display: "grid", gridTemplateColumns: "auto auto auto", gridTemplateRows: "auto", height: "400px"}} className={` ${styles.bg} items-start gap-4 bg-fuchsia-200 rounded-bl-3xl`}>
+      <div style={{display: "grid", gridTemplateColumns: "auto 1fr auto", gridTemplateRows: "auto", height: "400px"}} className={` ${styles.bg} items-start gap-4 bg-fuchsia-200 rounded-bl-3xl`}>
         {left}
         {right}
       </div>
--- src/components/PlayPage/PlayPage.js
+++ src/components/PlayPage/PlayPage.js
@@ -94,7 +94,7 @@ const Playground = ({id}) => {
 
 const StartButton = () => {
   return (
-    <button className={`justify-self-end col-start-3 row-start-1 rounded max-w-min px-6 py-2 cursor-pointer border border-fuchsia-600 text-fuchsia-600 hover:bg-fuchsia-600 hover:text-white font-medium m-6 z-10`}>Start</button>
+    <button className={`justify-self-end col-start-3 row-start-1 m-6 rounded max-w-min px-6 py-2 cursor-pointer border border-fuchsia-600 text-fuchsia-600 hover:bg-fuchsia-600 hover:text-white font-medium`}>Start</button>
   );
 }
 
@@ -102,9 +102,9 @@ const StartButton = () => {
 const QuizTitleDescription = ({title, description, creator}) => {
 
   return (
-    <div className={`flex flex-col space-y-2 col-start-1 col-end-4 row-start-4 mb-24 m-6 z-10`}>
+    <div className={`flex flex-col space-y-2 justify-self-start col-span-full row-start-4 mb-24 mx-6`}>
       <span className={`font-extrabold text-3xl text-fuchsia-700`}>{title}</span>
-      <span className={`font-normal text-base text-fuchsia-600 prose prose-sm`}>{description}</span>
+      <span className={`font-normal text-base text-fuchsia-600 break-all prose prose-sm`}>{description}</span>
       <span className={`font-bold text-fuchsia-600 text-sm text-fuchsia-600 pt-6 items-end`}>{creator}</span>
     </div>
   );
